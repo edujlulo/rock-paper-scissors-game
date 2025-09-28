90adb8f98cbbe3521ae6b91bbf6dec9f6849c547
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2025-09-28 11:23:22 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -9,9 +9,25 @@ function App() {
         <p id="made-by-text">Made by: Eduardo Lulo</p>
       </header>
       <div id="board">
-        <div className="screen">O</div>
-        <div id="score-display">O</div>
-        <div className="screen">O</div>
+        <div className="screen" id="human-screen">
+          <div id="human-title">Human</div>
+          <button class="button">Rock</button>
+          <button class="button">Paper</button>
+          <button class="button">Scissor</button>
+        </div>
+        <div id="score-display">
+          <div id="score-title">Score</div>
+          <p>Human</p>
+          <input></input>
+          <p>Computer</p>
+          <input></input>
+        </div>
+        <div className="screen" id="computer-screen">
+          <div id="computer-title">Computer</div>
+          <button class="button">Rock</button>
+          <button class="button">Paper</button>
+          <button class="button">Scissor</button>
+        </div>
       </div>
     </div>
   );

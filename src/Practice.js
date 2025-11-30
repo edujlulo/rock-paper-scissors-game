cd5e2fcc210b7af21f04eaed6d0fd7778698191c
Eduardo Lulo <eduardo.lulo.m@gmail.com>
2025-11-30 11:52:41 +0000

    Updates

--- src/Practice.js
+++ src/Practice.js
@@ -1,19 +1,1 @@
-import { useState } from "react";
 
-export default function List() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
-
-  return (
-    <>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + Number(step))}>Agregar</button>
-      <button onClick={() => setCount(count - Number(step))}>Restar</button>
-      <input
-        type="number"
-        value={step}
-        onChange={(e) => setStep(e.target.value)}
-      ></input>
-    </>
-  );
-}
